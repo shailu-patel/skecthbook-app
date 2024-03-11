de6d7e107d419da1b95e9ed93c783a3518e4b595
shailender <shailendarshailu225@gmail.comm>
2024-03-11 21:08:09 +0530

    Added urls

---
+++ src/socket.js
@@ -0,0 +1,6 @@
+import { io } from "socket.io-client";
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://sketchbox-app-server.onrender.com/"
+    : "http://localhost:5000";
+export const socket = io(URL);
